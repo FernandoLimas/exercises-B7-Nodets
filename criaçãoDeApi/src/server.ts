91aa448c7b483ad20c11d2f6550ac2a0ec6ccb60
fernandolima <nando_filipe@hotmail.com>
2022-05-06 23:23:40 -0300

    ci: npm install

--- criaçãoDeApi/src/server.ts
+++ criaçãoDeApi/src/server.ts
@@ -1,17 +1,27 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const server = express();
 
+server.use(bodyParser.json());
+
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({extended: true}));
+
+server.get('/ping', (req: Request, res: Response) => {
+    res.status(200);
+    res.json('pong');
+})
 
 server.use((req: Request, res: Response) => {
     res.status(404);
     res.json({error: 'Endpoint não encontrado.'});
 });
 
-server.listen(process.env.PORT);
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
